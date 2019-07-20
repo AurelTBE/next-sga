963cfbc8619f1be8cf97dbb08967900b501cfd04
AurelTBE <aurelien.trouble@gmail.com>
2019-07-21 00:30:39 +0200

    Pre-PDF render

--- pages/resultats/[id].js
+++ pages/resultats/[id].js
@@ -1,25 +1,28 @@
 import Layout from '../../components/Layout.js';
 import fetch from 'isomorphic-unfetch';
+import { Document, Page } from 'react-pdf';
 
-const Post = props => (
+const Resultat = props => (
   <Layout>
-    <h1>{props.post.title.rendered}</h1>
-    <p>{props.post.content.rendered.replace(/<[/]?p>/g, '')}</p>
-    <p>{props.post.acf.fichier_de_resultat_n1 ? props.post.acf.fichier_de_resultat_n1 : null}</p>
-    {/*<img src={props.post.image.medium} />*/}
+    <h1>{props.result.title.rendered}</h1>
+    <p>{props.result.content.rendered.replace(/<[/]?p>/g, '')}</p>
+    <p>{props.result.acf.fichier_de_resultat_n1 ? props.result.acf.fichier_de_resultat_n1 : null}</p>
+    {/*<Document file={props.result.acf.fichier_de_resultat_n1} >
+      <Page pageNumber={1} />
+    </Document>*/}
   </Layout>
 );
 
-Post.getInitialProps = async function(context) {
+Resultat.getInitialProps = async function(context) {
   const { id } = context.query;
   const res = await fetch(`http://sga-gymfeminine.fr/bo/wp-json/wp/v2/resultats?slug=${id}`);
-  const post = await res.json();
+  const result = await res.json();
 
-  console.log(post[0]);
+  console.log(result[0]);
 
   return {
-    post: post[0]
+    result: result[0]
   };
 };
 
-export default Post;
+export default Resultat;
