455c4fd3041f808bc465dbc5c4dfea5f73a347ce
AurelTBE <aurelien.trouble@gmail.com>
2019-07-21 17:35:46 +0200

    Multipage PDF display

--- components/PDFview.js
+++ components/PDFview.js
@@ -1,42 +1,65 @@
 import React, { Component } from "react";
-import { Document, Page, pdfjs } from "react-pdf";
+import { PDFDownloadLink, Document, Page, pdfjs } from "react-pdf";
+
+// Style
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-export default class PDFview extends Component {
-    state = { numPages: null, pageNumber: 1 };
-  
-    onDocumentLoadSuccess = ({ numPages }) => {
-      this.setState({ numPages });
+class PDFdoc extends Component {
+    state = {
+        numPages: null,
+      }
+    
+    onDocumentLoadSuccess = (document) => {
+        const { numPages } = document;
+        this.setState({
+          numPages,
+        });
     };
   
-    goToPrevPage = () =>
-      this.setState(state => ({ pageNumber: state.pageNumber - 1 }));
-    goToNextPage = () =>
-      this.setState(state => ({ pageNumber: state.pageNumber + 1 }));
-  
     render(props) {
-      const { pageNumber, numPages } = this.state;
-  
-      return (
-        <div>
-          <nav>
-            <button onClick={this.goToPrevPage}>Prev</button>
-            <button onClick={this.goToNextPage}>Next</button>
-          </nav>
-  
-          <div style={{ width: 800 }}>
-            <Document
-              file={this.props.pdf}
-              onLoadSuccess={this.onDocumentLoadSuccess}
-            >
-              <Page pageNumber={pageNumber} width={800} />
-            </Document>
-          </div>
-  
-          <p>
-            Page {pageNumber} of {numPages}
-          </p>
-        </div>
+        const { numPages } = this.state;
+
+        return (                    
+        <Document
+            file={this.props.pdf}
+            onLoadSuccess={this.onDocumentLoadSuccess}
+        >
+            {Array.from(
+                new Array(numPages),
+                (el, index) => (
+                    <Page
+                    key={`page_${index + 1}`}
+                    pageNumber={index + 1}
+                    width={800}
+                    />
+                ),
+            )}
+        </Document>
       );
     }
   }
+
+  const DLPdf = props => (
+    <div>
+      <PDFDownloadLink document={<PDFdoc pdf={props.pdf} />} fileName="resultats.pdf">
+        {({ blob, url, loading, error }) => (loading ? 'Chargement du document...' : 'Télécharger')}
+      </PDFDownloadLink>
+    </div>
+  )
+
+  
+export default function PDFview(props) {
+    return (
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <Grid container spacing={1} direction="column" alignItems="center">
+                    <PDFdoc pdf={props.pdf}/>
+                </Grid>
+            </Grid>
+        </Grid>
+      )
+}
