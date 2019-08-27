f1b1872dc26dac5bb7f4657f65cef9be379ff383
AurelTBE <aureltrouble@gmail.com>
2019-08-27 16:22:39 +0200

    Indent CardBenev

--- components/CardBenevole.js
+++ components/CardBenevole.js
@@ -53,76 +53,75 @@ export default function CardBenevole(props) {
     <Card className={classes.card}>
       <CardContent>
         <Grid container spacing={2}>
-            <Grid item>
-        <Avatar alt={benevoleFullName} src={benevole.acf.photo_du_benevole ? benevole.acf.photo_du_benevole : "/static/logo-sga.svg"} className={labelProps.size==="large" ? classes.bigAvatar : classes.smallAvatar} />
-        </Grid>
-        <Grid item xs container>
+          <Grid item>
+            <Avatar alt={benevoleFullName} src={benevole.acf.photo_du_benevole ? benevole.acf.photo_du_benevole : "/static/logo-sga.svg"} className={labelProps.size==="large" ? classes.bigAvatar : classes.smallAvatar} />
+          </Grid>
+          <Grid item xs container>
             <Grid item xs container direction="column" spacing={2}>
               <Grid item xs>
                 <Typography variant="h5" component="h2">
                   {benevoleFullName}
                 </Typography>
-        {
-            benevole.acf.fonction.map((fonction) => {
-                switch(fonction) {
-                  case 'Coach':
-                    return (
-                      <div key={fonction}>
-                        <Typography className={classes.pos} color="textSecondary">
-                          {fonction}
-                        </Typography>
-                        {benevole.acf.groupe_entraine.map((groupe) => {
-                          return <Typography variant="body2" component="span" key={groupe}> {groupe}</Typography>
-                        })}
-                      </div>
-                    );
-                  case 'Juge':
-                    return (
-                      <div key={fonction}>
-                        <Typography className={classes.pos} color="textSecondary">
-                          {fonction}
-                        </Typography>
-                        <Typography variant="body2" component="span">{benevole.acf.niveau_de_formation_juge}</Typography>
-                      </div>
-                    );
-                  case 'Bureau':
-                    return (
-                      <div key={fonction}>
-                        <Typography className={classes.pos} color="textSecondary">
-                          {fonction}
-                        </Typography>
-                        <Typography variant="body2" component="span"> {benevole.acf.role}</Typography>
-                      </div>
-                    );
-                  case 'Gymnaste':
-                    return (
-                      <div key={fonction}>
-                        <Typography className={classes.pos} color="textSecondary">
-                          {fonction}
-                        </Typography>
-                        {benevole.acf.groupe_de_pratique.map((groupeprat) => {
-                          return <Typography variant="body2" component="span" key={groupeprat}> {groupeprat}</Typography>
-                        })}
-                      </div>
-                    );
-                  default:
-                    return (
-                      <Typography className={classes.pos} color="textSecondary">
-                        {fonction}
-                      </Typography>
-                    );
+                {
+                  benevole.acf.fonction.map((fonction) => {
+                    switch(fonction) {
+                      case 'Coach':
+                        return (
+                          <div key={fonction}>
+                            <Typography className={classes.pos} color="textSecondary">
+                              {fonction}
+                            </Typography>
+                            {benevole.acf.groupe_entraine.map((groupe) => {
+                              return <Typography variant="body2" component="span" key={groupe}> {groupe}</Typography>
+                            })}
+                          </div>
+                        );
+                      case 'Juge':
+                        return (
+                          <div key={fonction}>
+                            <Typography className={classes.pos} color="textSecondary">
+                              {fonction}
+                            </Typography>
+                            <Typography variant="body2" component="span">{benevole.acf.niveau_de_formation_juge}</Typography>
+                          </div>
+                        );
+                      case 'Bureau':
+                        return (
+                          <div key={fonction}>
+                            <Typography className={classes.pos} color="textSecondary">
+                              {fonction}
+                            </Typography>
+                            <Typography variant="body2" component="span"> {benevole.acf.role}</Typography>
+                          </div>
+                        );
+                      case 'Gymnaste':
+                        return (
+                          <div key={fonction}>
+                            <Typography className={classes.pos} color="textSecondary">
+                              {fonction}
+                            </Typography>
+                            {benevole.acf.groupe_de_pratique.map((groupeprat) => {
+                              return <Typography variant="body2" component="span" key={groupeprat}> {groupeprat}</Typography>
+                            })}
+                          </div>
+                        );
+                      default:
+                        return (
+                          <Typography className={classes.pos} color="textSecondary">
+                            {fonction}
+                          </Typography>
+                        );
+                    }
+                  })
                 }
-              }      
-          )
-        }
-        {benevole.acf.informations_sur_le_benevole ?
-        <Typography variant="body2" component="p">
-          {benevole.acf.informations_sur_le_benevole}
-        </Typography>
-        : null}
-        </Grid>
-        </Grid>
-        </Grid>
+                {benevole.acf.informations_sur_le_benevole ?
+                  <Typography variant="body2" component="p">
+                    {benevole.acf.informations_sur_le_benevole}
+                  </Typography>
+                : null}
+              </Grid>
+            </Grid>
+          </Grid>
         </Grid>
       </CardContent>
     </Card>
