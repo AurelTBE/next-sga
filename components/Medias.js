f86da2a5ceadfef2cc2dc7a7388448bff1f09e55
AurelTBE <aureltrouble@gmail.com>
2019-08-31 19:33:22 +0200

    Mediatheque + custom endpoint

--- components/Medias.js
+++ components/Medias.js
@@ -1,15 +1,44 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+
 import Link from 'next/link';
 
 // MUI
 import Grid from '@material-ui/core/Grid';
 
-const Medias = props => (
-  <>
-    <Grid container justify="center" alignItems="stretch" alignContent="center" spacing={2}>
-      Medias
-      {/*console.log(props.mediafolders)*/}
-    </Grid>
-  </>
-);
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+}));
 
-export default Medias;
+export default function Media(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <GridList cellHeight={450} >
+        {props.mediafolders.map(mediafolder => (
+          <GridListTile key={mediafolder.id}>
+            <img src={mediafolder.couverture} alt={mediafolder.title} />
+            <GridListTileBar title={mediafolder.title} />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,8 @@ Index.getInitialProps = async function() {
   const act = await fetch(`${path}/sga/v1/posts`);
   const actus = await act.json();
 
-  const res = await fetch(`${path}/sga/v1/resultats`);
-  const resultats = await res.json();
+  const publicmedia = await fetch(`${path}/sga/v1/mediatheque`);
+  const publicmediafolders = await publicmedia.json();
 
   const eve = await fetch(`${path}/tribe/events/v1/events`);
   const events = await eve.json();
@@ -24,15 +24,15 @@ Index.getInitialProps = async function() {
   const alleve = await fetch(`${path}/tribe/events/v1/events?start_date=2018-06-01&end_date=2030-07-01`);
   const allevents = await alleve.json();
 
-  const allmedia = await fetch(`${path}/realmedialibrary/v1/`);
-  const allmediafolders = await allmedia.json();
+  const res = await fetch(`${path}/sga/v1/resultats`);
+  const resultats = await res.json();
 
   return {
     actus: actus,
     results: resultats,
     events: events.events,
     allevents: allevents.events,
-    mediafolders: allmediafolders,
+    mediafolders: publicmediafolders,
   };
 
 };
