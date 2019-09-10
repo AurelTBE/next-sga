a9d608685fd6e2fbfbc981bb312e1a62e8a1aefe
AurelTBE <aureltrouble@gmail.com>
2019-09-10 20:58:51 +0200

    Benevole card text color

--- components/CardBenevole.js
+++ components/CardBenevole.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
 import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBirthdayCake } from '@fortawesome/free-solid-svg-icons';
 
 // Media Query
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   card: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+    padding: 0,
   },
   title: {
     fontSize: 14,
@@ -43,11 +43,28 @@ const useStyles = makeStyles({
   fct: {
     marginBottom: 0,
   },
-});
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+    padding: 0,
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+    padding: 0,
+  },
+}));
 
 export default function CardBenevole(props) {
   const classes = useStyles();
   const theme = useTheme();
+  const [expanded, setExpanded] = React.useState(false);
+
+  function handleExpandClick() {
+    setExpanded(!expanded);
+  }
 
   const isSmallScreen = useMediaQuery(theme.breakpoints.down('xs'));
   const labelProps = {
@@ -59,20 +76,11 @@ export default function CardBenevole(props) {
   const benevoleFullName = `${infos.prenom} ${infos.nom}`;
 
   return (
-    <Card className={classes.card}>
+    <Card className={labelProps.size==="large" ? null : classes.card}>
       <CardContent>
         <Grid container spacing={2}>
           <Grid item>
             <Avatar alt={benevoleFullName} src={infos.photo_du_benevole ? infos.photo_du_benevole : "/static/logo-sga.svg"} className={labelProps.size==="large" ? classes.bigAvatar : classes.smallAvatar} />
-            <Grid container justify="center">
-              {infos.date_anniv ? 
-                <Hidden smUp>
-                  <Typography className={classes.pos} variant="body2" color="textSecondary">
-                    <FontAwesomeIcon icon={faBirthdayCake} className={classes.icons} /> {infos.date_anniv}
-                  </Typography>
-                </Hidden>
-               : null}
-            </Grid>
           </Grid>
           <Grid item xs container> 
             <Grid item xs container direction="column" spacing={2}>
@@ -80,15 +88,15 @@ export default function CardBenevole(props) {
                 <Typography variant="h5" component="h2" color="primary">
                   {benevoleFullName}
                 </Typography>
-                {infos.date_anniv ? <Hidden xsDown><Typography className={classes.pos} variant="body2" color="textSecondary"><FontAwesomeIcon icon={faBirthdayCake} className={classes.icons} /> {infos.date_anniv}</Typography></Hidden> : null}
-                {
+                {infos.date_anniv ? <Typography className={classes.pos} variant="body2" color="textSecondary"><FontAwesomeIcon icon={faBirthdayCake} className={classes.icons} /> {infos.date_anniv}</Typography> : null}
+                <Hidden xsDown>{
                   infos.fonction.map((fonction) => {
                     switch(fonction) {
                       case 'Coach':
                         return (
                           <div key={fonction}>
                             <Typography className={classes.pos} variant="body2" className={classes.fct}>
-                              <b>{fonction}</b> *{infos.niveau_de_formation_entraineur}* :{infos.groupe_entraine.map((groupe, index) => {
+                            <Typography className={classes.pos} component="span" variant="body2" color="secondary"><b>{fonction}</b></Typography> *{infos.niveau_de_formation_entraineur}* :{infos.groupe_entraine.map((groupe, index) => {
                               return <span key={fonction+groupe}> {groupe}{index < infos.groupe_entraine.length - 1 ? ',' : ''}</span>
                             })}
                             </Typography>
@@ -98,7 +106,7 @@ export default function CardBenevole(props) {
                         return (
                           <div key={fonction}>
                             <Typography className={classes.pos} variant="body2" className={classes.fct}>
-                            <b>{fonction}</b> *{infos.niveau_de_formation_juge}* :{infos.agres.map((agr, index) => {
+                            <Typography className={classes.pos} component="span" variant="body2" color="secondary"><b>{fonction}</b></Typography> *{infos.niveau_de_formation_juge}* :{infos.agres.map((agr, index) => {
                               return <span key={fonction+agr}> {agr}{index < infos.agres.length - 1 ? ',' : ''}</span>
                             })}
                             </Typography>
@@ -108,7 +116,7 @@ export default function CardBenevole(props) {
                         return (
                           <div key={fonction}>
                             <Typography className={classes.pos} variant="body2" className={classes.fct}>
-                            <b>{fonction}</b> *{infos.role}*
+                            <Typography className={classes.pos} component="span" variant="body2" color="secondary"><b>{fonction}</b></Typography> : {infos.role}
                             </Typography>
                           </div>
                         );
@@ -116,7 +124,7 @@ export default function CardBenevole(props) {
                         return (
                           <div key={fonction}>
                             <Typography className={classes.pos} variant="body2" className={classes.fct}>
-                            <b>{fonction}</b> :{infos.groupe_de_pratique.map((gpprat, index) => {
+                            <Typography className={classes.pos} component="span" variant="body2" color="secondary"><b>{fonction}</b></Typography> :{infos.groupe_de_pratique.map((gpprat, index) => {
                               return <span key={fonction+gpprat}> {gpprat}{index < infos.groupe_de_pratique.length - 1 ? ',' : ''}</span>
                             })}
                             </Typography>
@@ -140,12 +148,88 @@ export default function CardBenevole(props) {
                         );
                     }
                   })
-                }
+                }</Hidden>
+                <Hidden smUp>
+                  <IconButton
+                    className={clsx(classes.expand, {
+                      [classes.expandOpen]: expanded,
+                    })}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                  >
+                    <ExpandMoreIcon />
+                  </IconButton>
+                </Hidden>
               </Grid>
             </Grid>
           </Grid>
         </Grid>
       </CardContent>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          {
+            infos.fonction.map((fonction) => {
+              switch(fonction) {
+                case 'Coach':
+                  return (
+                    <div key={fonction}>
+                      <Typography className={classes.pos} variant="body2" className={classes.fct}>
+                      <Typography className={classes.pos} component="span" variant="body2" color="secondary"><b>{fonction}</b></Typography> *{infos.niveau_de_formation_entraineur}* :{infos.groupe_entraine.map((groupe, index) => {
+                        return <span key={fonction+groupe}> {groupe}{index < infos.groupe_entraine.length - 1 ? ',' : ''}</span>
+                      })}
+                      </Typography>
+                    </div>
+                  );
+                case 'Juge':
+                  return (
+                    <div key={fonction}>
+                      <Typography className={classes.pos} variant="body2" className={classes.fct}>
+                      <Typography className={classes.pos} component="span" variant="body2" color="secondary"><b>{fonction}</b></Typography> *{infos.niveau_de_formation_juge}* :{infos.agres.map((agr, index) => {
+                        return <span key={fonction+agr}> {agr}{index < infos.agres.length - 1 ? ',' : ''}</span>
+                      })}
+                      </Typography>
+                    </div>
+                  );
+                case 'Bureau':
+                  return (
+                    <div key={fonction}>
+                      <Typography className={classes.pos} variant="body2" className={classes.fct}>
+                      <Typography className={classes.pos} component="span" variant="body2" color="secondary"><b>{fonction}</b></Typography> *{infos.role}*
+                      </Typography>
+                    </div>
+                  );
+                case 'Gymnaste':
+                  return (
+                    <div key={fonction}>
+                      <Typography className={classes.pos} variant="body2" className={classes.fct}>
+                      <Typography className={classes.pos} component="span" variant="body2" color="secondary"><b>{fonction}</b></Typography> :{infos.groupe_de_pratique.map((gpprat, index) => {
+                        return <span key={fonction+gpprat}> {gpprat}{index < infos.groupe_de_pratique.length - 1 ? ',' : ''}</span>
+                      })}
+                      </Typography>
+                    </div>
+                  );
+                case 'Secrétaire':
+                  return (
+                    <div key={fonction}>
+                      <Typography className={classes.pos} variant="body2" className={classes.fct}>
+                      <b>{fonction}</b>
+                      </Typography>
+                    </div>
+                  );
+                default:
+                  return (
+                      <div key={fonction}>
+                          <Typography className={classes.pos} variant="body2" className={classes.fct}>
+                            <b>{fonction}</b>
+                          </Typography>
+                      </div>
+                  );
+              }
+            })
+          }
+        </CardContent>
+      </Collapse>
     </Card>
   );
 }
