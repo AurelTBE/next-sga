0668c601d564256c37eab7935fdf40f3da402fe3
AurelTBE <aureltrouble@gmail.com>
2019-09-12 22:57:27 +0200

    Benevole refactored

--- components/Benevoles.js
+++ components/Benevoles.js
@@ -1,12 +1,33 @@
 // MUI
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 // Composant
 import CardBenevole from '../components/CardBenevole';
 
-const Benevoles = props => (
+
+export default function Benevoles(props) {
+  const theme = useTheme();
+
+  return (
   <>
     <Grid container direction="row" justify="center" alignItems="stretch" spacing={2}>
+    <Grid item xs={12} sm={10}>
+    <Box
+                  display="flex" 
+                  bgcolor={theme.palette.primary.light}
+                  fontFamily="h6.fontFamily"
+                  fontSize={{ xs: 'h6.fontSize', md: 'h5.fontSize' }}
+                  p={{ xs: 2, sm: 3, md: 4 }}
+                  justifyContent="center"
+                  alignItems="center"
+                  height={{xs: 100, md: 120}}
+                >
+      <Typography component="h2" variant="h2" gutterBottom>
+          Bureau de la section Gym féminine :
+      </Typography></Box></Grid>
       {props.benevoles.map((benevole) => (
         <Grid item xs={12} lg={5} key={benevole.id}>
           <CardBenevole benevole={benevole} />
@@ -14,6 +35,4 @@ const Benevoles = props => (
       ))}
     </Grid>
   </>
-);
-
-export default Benevoles;
+)}
--- pages/gymfeminine.js
+++ pages/gymfeminine.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import SwipeableViews from 'react-swipeable-views';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
 
 // Components
 import Layout from '../components/Layout'
@@ -9,32 +15,93 @@ import Benevoles from '../components/Benevoles';
 
 import fetch from 'isomorphic-unfetch';
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
 const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing(3, 2),
+    backgroundColor: theme.palette.background.paper,
   },
 }));
 
-export default function GymFeminine(props) {
+export default function GymFem(props) {
   const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  function handleChangeIndex(index) {
+    setValue(index);
+  }
+
   return (
-      <Layout>
-        <Grid container justify="center" className={classes.root}>
-          <Grid item xs={10}>
-            <Typography component="h1" variant="h2" gutterBottom>
-              Section féminine de la SGA
-            </Typography>
-            <Typography component="h2" variant="h2" gutterBottom>
-              Bénévoles :
-            </Typography>
-          </Grid>
+    <Layout>
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="Présentation de la section féminine de gym de la SGA"
+        >
+          <Tab label="Entrainements" {...a11yProps(0)} />
+          <Tab label="Bénévoles" {...a11yProps(1)} />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          Entrainements
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
           <Benevoles benevoles={props.benevoles} />
-        </Grid>
-      </Layout>
-    )
+        </TabPanel>
+      </SwipeableViews>
+    </div>
+    </Layout>
+  );
 }
 
-GymFeminine.getInitialProps = async function() {
+//
+
+
+
+GymFem.getInitialProps = async function() {
   const res = await fetch(`http://sga-gymfeminine.fr/bo/wp-json/sga/v1/benevoles`);
   const benevoles = await res.json();
 
