032cc73b6eb7e345f94783cad61d2f5980ba9868
AurelTBE <aureltrouble@gmail.com>
2019-09-16 13:24:39 +0200

    Standard subscribe

--- pages/inscription.js
+++ pages/inscription.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -10,6 +10,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
 import Layout from '../components/Layout'
+import axios from 'axios';
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -42,6 +43,37 @@ const useStyles = makeStyles(theme => ({
 export default function Inscription() {
   const classes = useStyles();
 
+  const [display_name, setDisplayname] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const insertData = nonce => {
+    axios.post(`http://sga-gymfeminine.fr/bo/api/user/register/?username=${username}&email=${email}&nonce=${nonce}&display_name=${display_name}&user_pass=${password}&insecure=cool`)
+    .then(res => {
+      console.log('User inserted !', res.data)
+    }).catch(error => {
+      console.log(error.response)
+    })
+  }
+
+  const getWPnonce = () => {
+    axios.get('http://sga-gymfeminine.fr/bo/api/get_nonce/?controller=user&method=register')
+    .then(res => {
+      console.log(res.data)
+      insertData(res.data.nonce)
+    }).catch(error => {
+      console.log(error.response)
+    })
+  }
+  const handleSubmit = e => {
+    e.preventDefault();
+    // console.log('login with ', { username, password });
+    const user = { username, email, display_name, password };
+    getWPnonce()
+    console.log('Inscrit avec ', user)
+  };
+
   return (
     <Layout>
       <Container component="main" maxWidth="xs">
@@ -53,16 +85,44 @@ export default function Inscription() {
           <Typography component="h1" variant="h5">
             Inscription
           </Typography>
-          <form className={classes.form} noValidate>
+          <form className={classes.form} noValidate onSubmit={handleSubmit}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="nom"
+              label="Nom"
+              name="nom"
+              autoComplete="name"
+              value={display_name}
+              onChange={e => setDisplayname(e.target.value)}
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="pseudo"
+              label="Pseudo"
+              name="pseudo"
+              autoComplete="username"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+              autoFocus
+            />
             <TextField
               variant="outlined"
               margin="normal"
               required
               fullWidth
               id="email"
-              label="Adresse email"
+              label="Email"
               name="email"
               autoComplete="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
               autoFocus
             />
             <TextField
@@ -74,6 +134,8 @@ export default function Inscription() {
               label="Mot de passe"
               type="password"
               id="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
               autoComplete="current-password"
             />
             <Button
