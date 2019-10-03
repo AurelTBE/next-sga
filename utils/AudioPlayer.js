79d4d1d32cf754be4a5abdc2c7cf80a1a80bde7e
AurelTBE <aureltrouble@gmail.com>
2019-10-03 15:43:51 +0200

    Music galerie to finish

--- utils/AudioPlayer.js
+++ utils/AudioPlayer.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import { PlayButton, Timer } from 'react-soundplayer/components';
+import { PlayButton, Progress, VolumeControl, Timer, Icons } from 'react-soundplayer/components';
 
 // it's just an alias for 'withSoundCloudAudio' but makes code clearer
 import { withCustomAudio } from 'react-soundplayer/addons';
 
-// some track meta information
-const trackTitle = 'Great song by random artist';
-
 const AWSSoundPlayer = withCustomAudio(props => {
-  const { trackTitle } = props;
+  const { trackTitle, streamUrl } = props;
 
   return (
     <div>
       <PlayButton {...props} />
       <h2>{trackTitle}</h2>
+      <VolumeControl
+        className={String}
+        buttonClassName={String}
+        rangeClassName={String}
+        volume={Number} // in range 0-1
+        onVolumeChange={Function}
+        onToggleMute={Function}
+        soundCloudAudio={streamUrl} />
       <Timer {...props} />
+      <Progress
+        className={String}
+        innerClassName={String}
+        value={Number} // in range 0-100
+        onSeekTrack={Function}
+        soundCloudAudio={streamUrl} />
     </div>
   );
 });
