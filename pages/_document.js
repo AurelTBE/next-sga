16e59650ac673ec377b0e35fe1faf92c8642ba20
AurelTBE <aureltrouble@gmail.com>
2019-10-19 19:41:22 +0200

    PWA v2.1

--- pages/_document.js
+++ pages/_document.js
@@ -16,11 +16,10 @@ class MyDocument extends Document {
           />
           {/* Favicon */}
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="manifest" href="/public/static/manifest.json" />
+          <link rel="manifest" href="/static/manifest.json" />
           <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
-          <link rel="manifest" href="/static/site.webmanifest" />
           <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#d32f2f" />
           <link rel="shortcut icon" href="/static/favicon.ico" />
           <meta name="msapplication-TileColor" content="#d32f2f" />
