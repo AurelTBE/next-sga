667c9ee91a0c477c17a3d1a12670b5d5769981d7
AurelTBE <aureltrouble@gmail.com>
2019-11-10 13:48:52 +0100

    Rollback 5

--- pages/_app.js
+++ pages/_app.js
@@ -24,15 +24,6 @@ class MyApp extends App {
     if (jssStyles) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js').then(registration => {
-          console.log('SW registered: ', registration);
-        }).catch(registrationError => {
-          console.log('SW registration failed: ', registrationError);
-        });
-      });
-    }
   }
 
   render() {
