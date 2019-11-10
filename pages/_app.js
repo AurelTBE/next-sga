56b7fddba7d7aeafeb14c967002c71433305df68
AurelTBE <aureltrouble@gmail.com>
2019-11-10 13:17:48 +0100

    Custom SW 0.51

--- pages/_app.js
+++ pages/_app.js
@@ -24,6 +24,15 @@ class MyApp extends App {
     if (jssStyles) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(registration => {
+          console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+          console.log('SW registration failed: ', registrationError);
+        });
+      });
+    }
   }
 
   render() {
