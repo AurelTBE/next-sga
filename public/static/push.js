690bde249bfbe05fa1eb757c457d2eb24b7a972c
AurelTBE <aureltrouble@gmail.com>
2019-11-10 17:45:36 +0100

    Push listener attempt 0.4

--- public/static/push.js
+++ public/static/push.js
@@ -1,9 +1,28 @@
+'use strict';
+
 console.log("Coucou de push listener")
 
-self.addEventListener('push', (event) => {
-    const title = 'Get Started With Workbox';
+self.addEventListener('push', function(event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+  
+    const title = 'SGA News';
     const options = {
-      body: event.data.text()
+      body: 'Du nouveau sur la SGA',
+      icon: 'images/icon.png',
+      badge: 'images/badge.png'
     };
-    event.waitUntil(self.registration.showNotification(title, options));
-});
+  
+    const notificationPromise = self.registration.showNotification(title, options);
+    event.waitUntil(notificationPromise);
+  });
+
+  self.addEventListener('notificationclick', function(event) {
+    console.log('[Service Worker] Notification click Received.');
+  
+    event.notification.close();
+  
+    event.waitUntil(
+      clients.openWindow('https://sgagymfem.com')
+    );
+  });
