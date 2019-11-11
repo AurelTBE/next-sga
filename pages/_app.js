aaae2d69ea87435c86b57a92ad63ced58859f77b
AurelTBE <aureltrouble@gmail.com>
2019-11-11 14:31:20 +0100

    Push listener attempt 0.8

--- pages/_app.js
+++ pages/_app.js
@@ -24,16 +24,6 @@ class MyApp extends App {
     if (jssStyles) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/service-worker.js', { scope: '/' }).then(function (registration) {
-          console.log('Custom SW registered: ', registration)
-          registration.pushManager.subscribe({userVisibleOnly: true})
-        }).catch(function (registrationError) {
-          console.log('Custom SW registration failed: ', registrationError)
-        })
-      })
-    }
   }
 
   render() {
---
+++ utils/NotificationPermReq.js
@@ -0,0 +1,78 @@
+import React, {useState, useEffect} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.background.paper,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+  snackbar: {
+    [theme.breakpoints.down('xs')]: {
+      bottom: 90,
+    },
+  },
+}));
+
+export default function FabIntegrationSnackbar() {
+  const classes = useStyles();
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+        setIsOpen(true)
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  function askPermission() {
+    return new Promise(function(resolve, reject) {
+      const permissionResult = Notification.requestPermission(function(result) {
+        resolve(result);
+      });
+  
+      if (permissionResult) {
+        permissionResult.then(resolve, reject);
+      }
+    })
+    .then(function(permissionResult) {
+      if (permissionResult !== 'granted') {
+        throw new Error('We weren\'t granted permission.');
+      }
+    });
+  }
+
+  return (
+    <>
+        <Snackbar
+            open={isOpen}
+            autoHideDuration={4000}
+            ContentProps={{
+            'aria-describedby': 'snackbar-fab-message-id',
+            }}
+            message={<span id="snackbar-fab-message-id">Voulez-vous recevoir notifications pour être au courant de la publication de résultats ou d'actualités ?</span>}
+            action={
+            <>
+            <Button color="inherit" size="small" onClick={() => askPermission}>
+                OK
+            </Button>
+            <Button color="inherit" size="small" onClick={() => setIsOpen(false)}>
+                Plus tard
+            </Button>
+            </>
+            }
+            className={classes.snackbar}
+        />
+    </>
+  );
+}
