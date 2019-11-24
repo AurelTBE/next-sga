1b0e87bf450ea98678e232df184724feb099a3ca
AurelTBE <aureltrouble@gmail.com>
2019-11-24 11:48:23 +0100

    Manual cache fix attempt 0.1

--- components/Medias.js
+++ components/Medias.js
@@ -1,39 +1,117 @@
 import React, { useState, useEffect } from 'react';
 import fetch from 'isomorphic-unfetch';
 import useSWR from 'swr';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+
 import Link from 'next/link';
 
-// MUI
-import Grid from '@material-ui/core/Grid';
+// Media Query
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
-// Composant
-import ResultCard from "./cards/CardResult"
+// Masonry
+import Masonry from 'react-masonry-css';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  masonryGrid: {
+    display: 'flex',
+    marginLeft: 0,
+    width: "100%",
+  },
+  masonryColumn: {
+    paddinLeft: 0,
+  },
+  image: {
+    position: "relative",
+    width: "100%",
+    height: "auto"
+  },
+  caption: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    color: theme.palette.background.paper,
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    paddingBottom: 10,
+    paddingTop: 10,
+    width: "100%",
+  },
+  captiontext: {
+    paddingLeft: 10,
+  },
+  icon: {
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    transform: "translate(-50%, -50%)",
+    color: theme.palette.background.paper,
+    fontSize: 60,
+    [theme.breakpoints.down('sm')]: {
+      fontSize: 38,
+    },
+  },
+}));
 
 const fetcher = url => fetch(url).then(r => r.json())
 
-function Resultats() {
+export default function Media(props) {
+  const classes = useStyles();
+  const theme = useTheme();
   const [data, setData] = useState();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch(`https://sga-gymfeminine.fr/bo/wp-json/sga/v1/listresults`);
-      const results = await res.json();
-      setData(results);
+      const med = await fetch(`https://sga-gymfeminine.fr/bo/wp-json/sga/v1/mediatheque`);
+      const medias = await med.json();
+      setData(medias);
     };
     fetchData();
   }, []);
-  
-  if (!data) return <div>Chargement des résultats...</div>
-  return (
-    <>
-      <Grid container justify="center" alignItems="stretch" alignContent="center" spacing={2}>
-        {data.map((resultat) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} key={resultat.id}>
-            <ResultCard id={resultat.id} slug={resultat.slug} titre={resultat.title} img={resultat.couverture ? resultat.couverture : null} />
-          </Grid>
-        ))}
-      </Grid>
-    </>
-  )
-};
 
-export default Resultats;
+  const breakpointColumnsObj = {
+    default: 5,
+    1750: 4,
+    959: 3,
+    500: 2
+  };
+
+  if (!data) return <div>Chargement des galeries...</div>
+  return (
+    <div className={classes.root}>
+      <Masonry breakpointCols={breakpointColumnsObj} className={classes.masonryGrid} columnClassName={classes.masonryColumn}>
+        {data.map(mediafolder => {
+          if(mediafolder.media == "Photos") {
+            return (
+              <Link href="/medias/[id]" as={`/medias/${mediafolder.slug}`} key={mediafolder.id}>
+                <ButtonBase component="a">
+                  <img src={mediafolder.couverture} alt={mediafolder.title} className={classes.image} />
+                  <Box className={classes.caption} width={1}><Typography variant={ isSmallScreen ? "subtitle2" : "h6" } className={classes.captiontext}>{mediafolder.title}</Typography></Box>
+                </ButtonBase>
+              </Link>
+            )}            
+          else if(mediafolder.media == "Vidéos") {
+            return (
+              <Link href="/medias/[id]" as={`/medias/${mediafolder.slug}`} key={mediafolder.id}>
+                <ButtonBase component="a">
+                  <img src={mediafolder.couverture} alt={mediafolder.title} className={classes.image} />
+                  <Box className={classes.caption} width={1}><Typography variant={ isSmallScreen ? "subtitle2" : "h6" } className={classes.captiontext}>{mediafolder.title}</Typography></Box>
+                  <PlayCircleOutlineIcon className={classes.icon} />
+                </ButtonBase>
+              </Link>
+            )
+          }  
+        })}
+      </Masonry>
+    </div>
+  );
+}
